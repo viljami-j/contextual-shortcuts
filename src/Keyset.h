584793b543a394875896d5c7585a33c2deae2cfb
Viljami Mäkelä <viljami.j.m@gmail.com>
2022-12-12 04:21:06 +0200

    Nearly at alpha, unversioned

---
+++ src/Keyset.h
@@ -0,0 +1,93 @@
+﻿#pragma once
+
+#include <string>
+#include <map>
+#include <vector>
+#include <tinyxml2.h>
+#include <string>
+#include "CLog.hpp"
+
+using namespace tinyxml2;
+
+struct Shortcut
+{
+    std::string name;
+    std::vector<std::string> keybinds;
+
+    Shortcut() {};
+    Shortcut(std::string name, std::vector<std::string> keybinds) {
+        this->name = name;
+        this->keybinds = keybinds;
+    }
+};
+
+struct Context {
+
+    std::string name;
+    std::vector<Shortcut> shortcuts;
+
+    Context();
+    Context(XMLHandle contextElement) {
+        XMLHandle shortcutElement = contextElement.FirstChildElement("Shortcut");
+        name = contextElement.ToElement()->FirstAttribute()->Value();
+
+        // Load all shortcuts
+        for (XMLHandle e = shortcutElement; e.ToElement() != nullptr; e = e.NextSiblingElement("Shortcut")) {
+            // Get first keyElement of current shortcutElement
+            XMLHandle keyElement = e.FirstChildElement();
+            
+            std::string shortcutName = e.ToElement()->FirstAttribute()->Value();
+            printf("INFOR - ShortcutName: %s\n", shortcutName.c_str());
+
+            std::vector<std::string> keybind;
+
+            for (XMLHandle e = keyElement; e.ToElement() != nullptr; e = e.NextSiblingElement()) {
+                std::string key = e.ToElement()->Name();
+                printf("Key: %s\n", key.c_str());
+
+                keybind.push_back(key);
+            }
+
+            shortcuts.push_back(Shortcut(shortcutName, keybind));
+        }
+    }
+};
+
+
+class Keyset
+{
+private:
+    void ReloadContexts();
+    XMLDocument doc;
+
+public:
+    std::map<std::string, Context> contexts;
+
+    Keyset() {};
+
+    /* Opens a keyset from the specified filepath **/
+    Keyset(std::string filepath);   
+
+    bool LoadKeyset(std::string filepath) {
+        if (doc.LoadFile(filepath.c_str()) == XML_SUCCESS) {
+            ReloadContexts();
+            for (auto context : contexts) {
+                for (auto shortcut : context.second.shortcuts) {
+                    printf("\nOUTPUTTED ShortcutName: %s\n", shortcut.name.c_str());
+                    for (auto keybind : shortcut.keybinds) {
+                        printf("\nOUTPUTTED Key: %s\n", keybind.c_str());
+                    }
+                }
+            }
+
+            return true;
+        }
+        else CLog::ToChannels(doc.ErrorIDToName(doc.ErrorID()), CLog::Error);
+
+        return false;
+    }
+    
+
+    
+
+};
--- src/contextual-shortcuts.cpp
+++ src/contextual-shortcuts.cpp
@@ -2,9 +2,50 @@
 //
 
 #include <iostream>
-#include <tinyxml2.h>
+#include <fstream>
+#include "Keyset.h"
+#include "CLog.hpp"
+
+using namespace std;
+
+struct ProgramState
+{
+    Keyset Keyset;
+    bool isKeysetLoaded;
+
+    string ContextName;
+    bool isContextLoaded;
+
+    ProgramState() {};
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ProgramState ps;
+    // Check if a file exists
+    if (auto infile = ifstream("cshorts.txt"))
+    {
+        string line;
+
+        // Try to load keyset
+        getline(infile, line); 
+
+        if (ps.isKeysetLoaded = ps.Keyset.LoadKeyset(line)) {
+
+        }
+
+        if (ps.isKeysetLoaded) {
+            getline(infile, line);
+            auto iter = ps.Keyset.contexts.find(ps.ContextName);
+
+            for (auto shortcut : iter->second.shortcuts) {
+
+            }
+        }
+
+        infile.close();
+    }
+    else CLog::ToChannels("Failed to open cshorts.txt", CLog::Error);
+
+    return 0;
 }
